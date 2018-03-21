0a3610d62f2df2b47dd6562c4512e6ac716fe002
Hugo Campos <hugofcampos@gmail.com>
2018-03-21 10:17:33 -0300

    Fix key repeat speed

--- osx.sh
+++ osx.sh
@@ -152,7 +152,8 @@ defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 
 # Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+defaults write NSGlobalDomain InitialKeyRepeat -int 1
 
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 1
